54e363db83fd9d9b63da1e5ebe55e7873287b68b
vladimir komarov <komarov-krsk@mail.ru>
2016-05-11 17:23:49 +0700

    app monitor

--- apps/monitor/management/commands/ademco-officer.py
+++ apps/monitor/management/commands/ademco-officer.py
@@ -1,8 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
 
-from apps.monitor.models    import  dev_evt_log
-from apps.system.models import dir_device_type
+from apps.monitor.models    import  dev_evt_log,dev_evt_list
+from apps.system.models import dir_device_type,client_bind
 import time
+import datetime
 
 
 class Command(BaseCommand):
@@ -24,9 +25,25 @@ class Command(BaseCommand):
                 obj = int(mes[10:12],10)
                 src = int(mes[12:15],10)
 
+                try:
+                    item = client_bind.objects.get(console_number=panel)
+                    client_bind_id = item.id
+                except:
+                    client_bind_id = 0
+
+                rec = dev_evt_list.objects.get(device_type=device,evt_id=kod)
+
+
                 dev_evt_log.objects.create(device_id=panel,stub=src,zone=obj,message_id=kod,device_type=device,data={
                     'message':mes,
-                    'datetime' : time.time()
+                    'datetime' : time.time(),
+                    'date_text':datetime.datetime.now().strftime('%d.%m.%Y'),
+                    'time_text':datetime.datetime.now().strftime('%H:%M'),
+                    'message_text':rec.name,
+                    'alert_level':rec.alert_level,
+                    'stub_text':'',
+                    'zone_text':'',
+                    'client_bind_id':client_bind_id
                 })
 
                 print "ok"
