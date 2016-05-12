b337c87caebf855acceb43b3d314f993694a6634
vladimir komarov <komarov-krsk@mail.ru>
2016-05-12 14:44:28 +0700

    operator

--- apps/monitor/operator/operator.py
+++ apps/monitor/operator/operator.py
@@ -5,27 +5,43 @@ import datetime
 from django.http import HttpResponse
 from django.db import connections
 from apps.system.models  import client_bind
-
+from apps.system.models  import client_object_timetable
+import apps.settings
 
 
 def GetOperatorData(request):
 
 
-    client_bind_id = request.GET["client_bind"]
-
-    item = client_bind.objects.get(pk=client_bind_id)
-
-    print item
-
     response_data = {}
 
+    r = request.GET
+    rg = request.GET.get
 
 
-    response_data['phone'] = {}
-    response_data['docs'] = {}
-    response_data['timetable'] = {}
+    if r.has_key("client_bind") and rg("client_bind") != '':
+
+        client_bind_id = request.GET["client_bind"]
+
+        item = client_bind.objects.get(pk=client_bind_id)
+
+        users = item.client_object.client_user.all()
 
 
+        for t in client_object_timetable.objects.all():
+            if t.client_object == item.client_object:
+                print t
+
+
+        response_data['phone'] = {}
+        response_data['docs'] = {}
+        response_data['timetable'] = {}
+        response_data['additions'] = {u'client_name': client_bind.objects.get(pk=client_bind_id).client_contract.client.name }
+
+
+
+    if r.has_key("settings") and rg("settings") != '':
+
+        response_data['max_rows'] = apps.settings.OPERATOR_EVT_MAX_ROWS
 
 
 
