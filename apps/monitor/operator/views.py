9c6840b04aa60df5a48206ba71f534316ef3eb27
vladimir komarov <komarov-krsk@mail.ru>
2016-05-19 10:45:52 +0700

    Восстановление

--- apps/monitor/operator/views.py
+++ apps/monitor/operator/views.py
@@ -5,10 +5,44 @@ import datetime
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from apps.system.models  import client_bind
+from apps.system.models  import client_bind,dir_security_squad
+from apps.monitor.models import dev_evt_log,dev_result_list
+
+import apps.settings
 
 
-#from apps.system import models
+
+def GetGbrList():
+
+    data = ["ГБР не направлялся"]
+
+    for row in dir_security_squad.objects.filter(is_active=1).order_by("name"):
+        data.append(row.name)
+
+    return data
+
+
+
+def GetStartList():
+
+    data = []
+
+    for row in dev_result_list.objects.filter(result_type=0).order_by("name"):
+        data.append(row.name)
+
+    return data
+
+
+def GetEndList():
+
+    data = []
+
+    for row in dev_result_list.objects.filter(result_type=1).order_by("name"):
+        data.append(row.name)
+
+    return data
+
+
 
 
 
@@ -16,7 +50,12 @@ def main(request, client_id=None):
     if request.user.has_perm('sentry.monitor'):
 
 
+        gbr_list = GetGbrList()
+        start_list = GetStartList()
+        end_list = GetEndList()
+
         data = client_bind.objects.all()
+        data_log = dev_evt_log.objects.order_by('-datetime_evt')[0:apps.settings.OPERATOR_EVT_MAX_ROWS]
 
         return render_to_response('monitor/operator/monitor-operator.html', locals(), RequestContext(request))
 
